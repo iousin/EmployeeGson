325a06955ca29e3c262124a308f8d8543e6baadc
James.Toddington <james.toddington@underwriteme.co.uk>
2019-01-18 17:16:00 +0000

    Initial commit

---
+++ employee-gson/employee-gson-android/employee-gson-lib/src/main/java/example/com/employeegsonlib/Example.java
@@ -0,0 +1,11 @@
+package example.com.employeegsonlib;
+
+import com.employee.gson.EmployeeGsonExample;
+
+public class Example {
+
+    public Example() {
+        EmployeeGsonExample.convertEmployees(1);
+    }
+
+}
